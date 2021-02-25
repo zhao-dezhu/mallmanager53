5ebe8a5cee738cf1548a9b722187d635e2692da1
zhao-dezhu <1728168375@qq.com>
2021-02-25 16:10:52 +0800

    侧边导航组件

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -21,7 +21,78 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <!-- 侧边导航栏 -->
+        <el-menu :unique-opened="true">
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-user-solid"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-s-claim"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-user"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-s-order"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-s-goods"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-s-cooperation"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-tickets"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-collection"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-s-order"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-s-claim"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-s-marketing"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-document-copy"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">main</el-main>
     </el-container>
   </el-container>
