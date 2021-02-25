067afc7f56b8fc4389d678e85b0be9164d4ea347
zhao-dezhu <1728168375@qq.com>
2021-02-25 13:40:46 +0800

    引入提示框

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "@/assets/css/reset.css";
+import router from "./router";
+import MyServerHttp from "@/plugins/http.js";
+Vue.use(ElementUI);
+//开发axios插件
+Vue.use(MyServerHttp);
 
-Vue.use(ElementUI)
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
